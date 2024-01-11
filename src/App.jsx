2d5ed5217fbce9283662c4ed40ad12977e45feab
Olya <olapancisina1@gmail.com>
2024-01-11 13:45:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import BeckgroundHeading from './BeckgroundHeading'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <>
+      <BeckgroundHeading/>
+      <main></main>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/BeckgroundHeading.jsx
@@ -0,0 +1,6 @@
+
+export default function BeckgroundHeading() {
+	return (
+		<div>BeckgroundHeading</div>
+	)
+}
