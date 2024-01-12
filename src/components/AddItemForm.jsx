d898112de150e447ac7d4e928f1a32ed318c51a5
Olya <olapancisina1@gmail.com>
2024-01-12 11:06:06 +0100

    interactive for adding elements

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Button from './Button'
 
 export default function AddItemForm({ setItem }) {
 	const [itemText, setItemText] = useState('')
-	return (
-		<form
-			onSubmit={(e) => {
-				e.preventDefault()
+	const inputRef = useRef()
 
-				const newItem = {
-					id: new Date().getTime(),
-					name: itemText,
-					packed: false,
-				}
-				setItem((prev) => [...prev, newItem])
-			}}
-		>
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		if (itemText !== '') {
+			const newItem = {
+				id: new Date().getTime(),
+				name: itemText,
+				packed: false,
+			}
+			setItem(prev => [...prev, newItem])
+		} else {
+			alert("Item can't be empty")
+			inputRef.current.focus()
+		}
+		setItemText('')
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
 			<h2>Add an item</h2>
 			<input
 				value={itemText}
 				onChange={e => {
 					setItemText(e.target.value)
 				}}
+				ref={inputRef}
+				autoFocus
 			/>
 			<Button>Add to list</Button>
 		</form>
